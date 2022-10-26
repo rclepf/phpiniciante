c5a141dde1bc3e9321f04325ed11512897d44136
rclepf <rclepf@gmail.com>
2022-10-26 10:41:43 -0300

    putcontents

--- arquivos/escritorjogos.php
+++ arquivos/escritorjogos.php
@@ -1,9 +1,5 @@
 <?php
 
-$arquivo = fopen('novosjogos.txt', 'a');
+$jogo ="\n" . 'Ghost of Tsushima';
 
-$jogo ="\n" . 'Marvel Spider-Man Miles Morales';
-
-fwrite($arquivo, $jogo);
-
-fclose($arquivo);   
+file_put_contents('novosjogos.txt', $jogo, FILE_APPEND);
