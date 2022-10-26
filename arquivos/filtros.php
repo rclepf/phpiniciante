d96bb5f257c3fb930d5bddeb3c1f966d8ef4b33b
rclepf <rclepf@gmail.com>
2022-10-26 11:47:20 -0300

    meufiltro

--- arquivos/filtros.php
+++ arquivos/filtros.php
@@ -1,7 +1,10 @@
 <?php
 
-$listaJogos = fopen('jogosps5.txt','r');
+require 'MeuFiltro.php';
 
-stream_filter_append($listaJogos, 'string.toupper');
+$listaJogos = fopen('jogosps5.txt', 'r');
+
+stream_filter_register('jogos.call', MeuFiltro::class);
+stream_filter_append($listaJogos, 'jogos.call');
 
 echo fread($listaJogos, filesize('jogosps5.txt'));
