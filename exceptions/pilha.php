5384cdc8beb71844fe8c6047ef18e91901887c9c
rclepf <rclepf@gmail.com>
2022-10-27 16:54:24 -0300

    runtimesolution

--- exceptions/pilha.php
+++ exceptions/pilha.php
@@ -3,19 +3,24 @@
 function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
-    try{
-        $arrayFixo = new SplFixedArray(2);
-        $arrayFixo[3] = 'Valor';
+    /* try{
+       
     } catch (RuntimeException $problema) {
         echo "Aconteceu um erro na função 1" . PHP_EOL;
-    }
+    } */
+    try{
     funcao2();
+    } catch (RuntimeException $problema){
+        echo "A solução do problema esta na função 1" . PHP_EOL;
+    }
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[3] = 'Valor';
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }
