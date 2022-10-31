a8aee86f4108a0a6e12d7aeccd85fb3ae5e0f35e
rclepf <rclepf@gmail.com>
2022-10-31 15:57:52 -0300

    set errors

---
+++ exceptions/erros.php
@@ -0,0 +1,12 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', '/var/log/minha-aplicacao');
+
+
+echo $variavel;
+echo $array[12];
+
+echo CONSTANTE;
